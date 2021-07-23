505f5fe8a5627bb5a2b56c84ae927e1dba8de2fa
Felipe Ascari <felipe.ascari@gmail.com.br>
2021-07-23 14:58:42 -0300

    Camada de modelo e conexão com o h2 database

--- clientes-api/src/main/java/io/github/fascari/clientes/model/entity/Cliente.java
+++ clientes-api/src/main/java/io/github/fascari/clientes/model/entity/Cliente.java
@@ -1,23 +1,44 @@
 package io.github.fascari.clientes.model.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import javax.persistence.*;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.br.CPF;
+import com.fasterxml.jackson.annotation.JsonFormat;
 
 @Entity
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class Cliente {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(nullable = false, length = 150)
+    @NotEmpty(message = "{campo.nome.obrigatorio}")
     private String nome;
 
     @Column(nullable = false, length = 11)
+    @NotNull(message = "{campo.cpf.obrigatorio}")
+    @CPF(message = "{campo.cpf.invalido}")
     private String cpf;
 
-    @Column(name = "data_cadastro")
+    @Column(name = "data_cadastro", updatable = false)
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataCadastro;
+
+    @PrePersist
+    public void prePersist() {
+        dataCadastro = LocalDate.now();
+    }
 }
